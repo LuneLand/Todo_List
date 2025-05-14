e13e3da4f99acffd92d077610f5b228b7af94e03
LuneLand <razmanmahtab@gmail.com>
2025-05-14 05:53:09 -0700

    Update TodoItems.jsx

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -7,7 +7,7 @@ const TodoItems = ({text, id, isComplete, deleteTodo, toggle}) => {
     return (
         <div className='flex items-center my-3 gap-2'>
             <div onClick={()=>{toggle(id)}} className='flex flex-1 items-center cursor-pointer'>
-                <img src={isComplete ? tick : not_tick} alt="" className='w-12'/>
+                <img src={isComplete ? tick : not_tick} alt="" className='w-10 sm:12'/>
                 <p className={`text-slate-700 ml-4 text-[17px] decoration-slate-500
                     ${isComplete ? "line-through" : ""}`
                 }>{text}</p>
